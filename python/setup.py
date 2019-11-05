587422397e53bf3644ee955c9381f92ead439c7b
Paul Melnikow <email@paulmelnikow.com>
2019-11-05 09:19:02 -0500

    This seems to be working

--- python/setup.py
+++ python/setup.py
@@ -3,20 +3,45 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# https://github.com/pybind/python_example/blob/master/setup.py
+# Adapted from https://github.com/pybind/python_example/blob/master/setup.py
 class get_pybind_include(object):
     """Helper class to determine the pybind11 include path
     The purpose of this class is to postpone importing pybind11
     until it is actually installed, so that the ``get_include()``
     method can be invoked. """
 
-    def __init__(self, user=False):
+    def __init__(self, user=False, pep517=False):
         self.user = user
+        self.pep517 = pep517
 
     def __str__(self):
+        import os
         import pybind11
 
-        return pybind11.get_include(self.user)
+        interpreter_include_path = pybind11.get_include(self.user)
+
+        if self.pep517:
+            # When pybind11 is installed permanently in site packages, the headers
+            # will be in the interpreter include path above. pep517 provides an
+            # experimental feature for build system dependencies. When installing
+            # a package from a source distribvution, first its build dependencies
+            # are installed in a temporary location. pybind11 does not return the
+            # correct path for this condition, so we glom together a second path,
+            # and ultimately specify them _both_ in the include search path.
+            # https://github.com/pybind/pybind11/issues/1067
+            return os.path.abspath(
+                os.path.join(
+                    os.path.dirname(pybind11.__file__),
+                    "..",
+                    "..",
+                    "..",
+                    "..",
+                    "include",
+                    os.path.basename(interpreter_include_path),
+                )
+            )
+        else:
+            return interpreter_include_path
 
 
 # `tiny_obj_loader.cc` contains implementation of tiny_obj_loader.
@@ -27,9 +52,12 @@ m = setuptools.Extension(
     include_dirs=[
         # Make `build_ext` work without `sdist`.
         "..",
-        # Support `build_ext` as well as installs from source distribution.
+        # Support `build_ext` (provided pybind11 is installed).
         get_pybind_include(),
         get_pybind_include(user=True),
+        # Support building from a source distribution (pybind11 from
+        # a pep517 temporary install).
+        get_pybind_include(pep517=True),
     ],
     language="c++",
 )
